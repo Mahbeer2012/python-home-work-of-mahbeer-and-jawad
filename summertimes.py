b59d0caf37d6ad3ec0b0508b2a7dafb58e225120
mahbeer2012 <mahbeermubashir031@gmali.com>
2025-01-17 23:28:49 -0800

    c:\Users\Lenovo\Desktop\python home work of mahbeer and jawad\summertimes.py

---
+++ summertimes.py
@@ -0,0 +1,8 @@
+#get user input from the temprature
+temprature = float(input("enter the current temprature in degrees celsius"))
+
+#conditional statements
+if temprature > 25:
+    print("it's warm to wear light cloths.")
+else:
+    print("it's cold, better stick to warm cloths.")
